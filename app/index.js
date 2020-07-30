fd1fe513df7c104e6dea2fc4ee74c93d48e8f67f
Wechuli <wechulipaul@yahoo.com>
2020-07-30 11:12:33 +0300

    add calculator method

--- app/index.js
+++ app/index.js
@@ -8,3 +8,19 @@ const fruits = [
 ];
 
 console.log(fruits.length);
+
+const person = {
+  firstName: "Paul",
+  lastName: "Wechuli",
+  age: 27,
+  occupation: "Developer",
+  interests: ["Sports", "Technology", "Classic Literature"],
+};
+
+function calculateFrequency(list) {
+  const frequencyCounter = {};
+  for (let i = 0; i < list.length; i++) {
+    if (frequencyCounter[list[i]]) {
+    }
+  }
+}
