cfe0a296b2ecb47d4ee8e6d5150ffa2c2a693cc6
Wechuli <wechulipaul@yahoo.com>
2020-11-06 12:13:30 +0300

    code

--- __tests__/main.js
+++ __tests__/main.js
@@ -1,6 +1,10 @@
-const { add } = require("../app/adder");
+const { add, sub } = require("../app/adder");
 const { expect } = require("@jest/globals");
 
 test("should add two integers correctly", () => {
   expect(add(1, 2)).toBe(3);
+});
+
+test("should add two integers correctly", () => {
+  expect(sub(2, 1)).toBe(1);
 });
